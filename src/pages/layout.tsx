e9f2e090a0eb13816221720e571690672b987ffb
navelorange1999 <qichen1999@qq.com>
2024-10-10 15:34:25 +0800

    :green_heart: Try to fix ci failed

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,3 @@
-import type {Metadata} from 'next';
-
-export const metadata: Metadata = {
-	title: 'navelorange1999 blog',
-	description: 'navelorange1999 blog',
-};
-
 export default function RootLayout({
 	children,
 }: Readonly<{
